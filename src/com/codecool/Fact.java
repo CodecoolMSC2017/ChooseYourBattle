ff7368f782d3e00b10de62ad8ab07d18d45342ee
Laszlo Poroszkai <laszlo.poroszkai@gmail.com>
2018-02-06 12:46:17 +0100

    readd deleted files

---
+++ src/com/codecool/Fact.java
@@ -0,0 +1,14 @@
+package com.codecool;
+import java.util.List;
+
+public class Fact {
+    private String type;
+    private String name;
+    private String era;
+    private List<String> winners;
+    private List<String> losers;
+    private Integer length;
+    private Long casualities;
+
+
+}
