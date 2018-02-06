11b8efbfc27e59cd2bcfdfcddc541b6f1783930a
roskogabor <roskogabor@gmail.com>
2018-02-06 14:15:48 +0100

    answer class building: iterator added

--- src/com/codecool/RuleParser.java
+++ src/com/codecool/RuleParser.java
@@ -1,7 +1,26 @@
 package com.codecool;
 
+import java.util.Iterator;
+
 public class RuleParser {
+    Iterator<Question> iterator = new QuestionIterator();
+    Question question;
+
     public RuleRepository getRuleRepository() {
 
+    }
+
+    public Iterator<Question> getIterator() {
+        return iterator;
+    }
+
+    class QuestionIterator implements Iterator<Question> {
+        public boolean hasNext() {
+            return true;
+        }
+
+        public Question next() {
+            return question;
+        }
     }
 }
