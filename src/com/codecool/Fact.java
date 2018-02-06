3469a1ac2c65a011d9e0899a6b65af07afed23f7
Kontra László <kontravlaszlo@gmail.com>
2018-02-06 11:23:39 +0100

    Skeleton after restart

---
+++ src/com/codecool/Fact.java
@@ -0,0 +1,29 @@
+package com.codecool;
+
+import java.util.Set;
+
+public class Fact {
+    private String id;
+    private String description;
+
+    public Fact(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Set<String> getIdSet() {
+
+    }
+
+    public void setFactValueById(String id, boolean value) {
+
+    }
+
+    public boolean getValueById(String id) {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
---
+++ src/com/codecool/RuleParser.java
@@ -0,0 +1,7 @@
+package com.codecool;
+
+public class RuleParser {
+    public RuleRepository getRuleRepository() {
+
+    }
+}
