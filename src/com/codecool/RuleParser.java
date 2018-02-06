351392dfb91d214af547857d89b28c016ac3721e
roskogabor <roskogabor@gmail.com>
2018-02-06 14:27:41 +0100

    refactor

--- src/com/codecool/RuleParser.java
+++ src/com/codecool/RuleParser.java
@@ -3,24 +3,8 @@ package com.codecool;
 import java.util.Iterator;
 
 public class RuleParser {
-    Iterator<Question> iterator = new QuestionIterator();
-    Question question;
 
     public RuleRepository getRuleRepository() {
 
-    }
-
-    public Iterator<Question> getIterator() {
-        return iterator;
-    }
-
-    class QuestionIterator implements Iterator<Question> {
-        public boolean hasNext() {
-            return true;
-        }
-
-        public Question next() {
-            return question;
-        }
     }
 }
--- src/com/codecool/RuleRepository.java
+++ src/com/codecool/RuleRepository.java
@@ -3,11 +3,24 @@ package com.codecool;
 import java.util.Iterator;
 
 public class RuleRepository implements Iterator<Fact> {
+    Iterator<Question> iterator = new QuestionIterator();
+    Question question;
+    
     public void addQuestion(Question question) {
 
     }
 
     public Iterator<Question> getIterator() {
 
+    }
+
+    class QuestionIterator implements Iterator<Question> {
+        public boolean hasNext() {
+            return true;
+        }
+
+        public Question next() {
+            return question;
+        }
     }
 }
