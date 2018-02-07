204b1d02d383bba36c84c5ac1eb0d28bb566311f
roskogabor <roskogabor@gmail.com>
2018-02-07 16:38:35 +0100

    getrulerepository method is done in ruleparser waiting for test

--- src/com/codecool/RuleParser.java
+++ src/com/codecool/RuleParser.java
@@ -3,6 +3,9 @@ package com.codecool;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class RuleParser extends XMLParser {
@@ -16,18 +19,33 @@ public class RuleParser extends XMLParser {
             if (ruleNode.getNodeType() == Node.ELEMENT_NODE) {
                 Element ruleElement = (Element) ruleNode;
                 String questionId = ruleElement.getAttribute("id");
-                String question = ruleElement.getElementsByTagName("Question").item(0).getTextContent();
+                String newQuestion = ruleElement.getElementsByTagName("Question").item(0).getTextContent();
                 Node answerNode = ruleElement.getElementsByTagName("Answer").item(0);
+                List<Value> valueList = new ArrayList<>();
                 Element answerElement = (Element) answerNode;
                 NodeList selectionList = answerElement.getElementsByTagName("Selection");
                 for (int tempSelection = 0; tempSelection < selectionList.getLength(); tempSelection++) {
                     Node selectionNode = selectionList.item(tempSelection);
                     if (selectionNode.getNodeType() == Node.ELEMENT_NODE) {
                         Element selectionElement = (Element) selectionNode;
-                        
+                        NodeList singleValueList = selectionElement.getElementsByTagName("SingleValue");
+                        NodeList multipleValueList = selectionElement.getElementsByTagName("MultipleValue");
+                        if (singleValueList.getLength() > 0) {
+                            Value singleValue = new SingleValue(singleValueList.item(0).getTextContent());
+                            valueList.add(singleValue);
+                        } else {
+                            String multipleValues = multipleValueList.item(0).getTextContent();
+                            List<String> splittedMultipleValues = Arrays.asList(multipleValues.split(","));
+                            Value multipleValue = new MultipleValue(splittedMultipleValues);
+                            valueList.add(multipleValue);
+                        }
                     }
                 }
-                }
+                Answer answer = new Answer(valueList);
+                Question question = new Question(questionId, newQuestion, answer);
+                rules.addQuestion(question);
             }
+        }
+        return rules;
     }
 }
