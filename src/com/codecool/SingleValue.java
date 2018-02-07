901713f6d45b36a89abbe65fd26ae562a921bc1b
Laszlo Poroszkai <laszlo.poroszkai@gmail.com>
2018-02-07 14:28:33 +0100

    working on Values and evaluation

--- src/com/codecool/SingleValue.java
+++ src/com/codecool/SingleValue.java
@@ -1,5 +1,8 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SingleValue extends Value {
     private String singleValue;
 
@@ -12,5 +15,10 @@ public class SingleValue extends Value {
         return singleValue;
     }
 
+    public List<String> getInputPattern(){
+        List<String> validValues = new ArrayList<>();
+        validValues.add(singleValue);
+        return validValues;
+    }
 
 }
