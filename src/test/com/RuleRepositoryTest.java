cd7fc56efa0129b5e07d855b902cbb1e68f7871e
Kontra László <kontravlaszlo@gmail.com>
2018-02-08 11:39:57 +0100

    Did skeleton for RuleParserTest, ruleRepositoryTest, QuestionIteratorTest, AnswerTest

---
+++ src/test/com/RuleRepositoryTest.java
@@ -0,0 +1,38 @@
+package test.com;
+
+import com.codecool.Answer;
+import com.codecool.Question;
+import com.codecool.RuleRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleRepositoryTest {
+    RuleRepository ruleRepositoryTest = new RuleRepository();
+    Answer answer;
+    Question q;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addQuestion() {
+    }
+
+    @Test
+    void removeQuestion() {
+    }
+
+    @Test
+    void getIterator() {
+    }
+
+    @Test
+    void getQuestions() {
+    }
+}
