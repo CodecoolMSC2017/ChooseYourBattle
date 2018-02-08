6a576649ebe5451927f210eab8765c767e64410d
roskogabor <roskogabor@gmail.com>
2018-02-08 11:47:24 +0100

    added test skeleton

---
+++ src/com/codecool/FactParserTest.java
@@ -0,0 +1,16 @@
+package com.codecool;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class FactRepositoryTest {
+    @BeforeEach
+    void SetUp() {
+
+    }
+
+    @Test
+    void addQuestion() {
+
+    }
+}
